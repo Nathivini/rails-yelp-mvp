e5294e05ab648e2269380404c083aa9ad11b2f46
Nathalie Roth <nathaliemiriam.roth@gmail.com>
2022-05-18 16:37:17 +0100

    finishhed yeahhh

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+puro = { name: "Puro", address: "Rigitrasse 6", category: "japanese" }
+ramiro = { name: "Ramiro", address: "Avenida 7", category: "italian" }
+villa = { name: "Villa", address: "7Josefstrasse 202", category: "french" }
+tasca = { name: "Tasca", address: "Rua mia 7", category: "belgian" }
+
+restaurants = [dishoom, puro, ramiro, tasca, villa]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "#{restaurant.name} has been created"
+end
+
+puts "Finished"
